eef6905351cea4d6f590551feeed866bd6ff694d
Hitesh <hitesh.chaudhari@wohlig.com>
2019-09-27 10:42:55 +0530

    first git

---
+++ src/components/banner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mb-4 main-title text-waight-bold">
+    <div class="text">Yes,There's An Artist inside You</div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+.text {
+  background: url(https://media.giphy.com/media/3o6Ztb45EYezY9x9gQ/giphy.gif);
+  background-size: contain;
+  background-position: top left;
+  -webkit-background-clip: text;
+  color: transparent;
+  font-size: 5rem;
+  font-weight: bold;
+  font-family: sans-serif;
+}
+</style>
---
+++ src/views/Service.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <banner-Section />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import bannerSection from "@/components/banner.vue";
+
+export default {
+  name: "home",
+  components: {
+    bannerSection
+  }
+};
+</script>
